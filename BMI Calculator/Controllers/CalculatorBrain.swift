df1daddfcb3470c784f8426d33e4e52ddea57866
Carson Clark <carsonclark@mbp14.local>
2023-11-27 14:59:51 -0800

    Added advice value to inform user what to do given their current BMI

--- BMI Calculator/Controllers/CalculatorBrain.swift
+++ BMI Calculator/Controllers/CalculatorBrain.swift
@@ -11,6 +11,7 @@ import Foundation
 struct CalculatorBrain {
     
     var bmiValue: String?
+    var adviceValue: String?
     
     mutating func calculateBMI(height: Float, weight: Float) {
         // Extract the whole number
@@ -21,9 +22,22 @@ struct CalculatorBrain {
         
         let bmi = weight / pow(heightInInches, 2) * 703
         bmiValue = String(format: "%.1f", bmi)
+        
+        if bmi < 18.5 {
+            adviceValue = "You need to eat more!"
+        } else if bmi < 24.5 {
+            adviceValue = "You're va healthy weight"
+        } else {
+            adviceValue = "You are overweight."
+        }
     }
 
      func getBMIValue() -> String {
-       return bmiValue!
+         // return bmiValue and if it's nil return default value of 0.0 
+         return bmiValue ?? "0.0"
+    }
+    
+    func getAdviceValue() -> String {
+        return adviceValue ?? "No weight or heigh provided"
     }
 }
